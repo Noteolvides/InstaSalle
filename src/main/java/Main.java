8effcae53a9772cef31aec42d7b9b877b813b660
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-11-28 10:40:53 +0100

    Added Gson Parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+
 public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().create();
+        InfoJson informacion = gson.fromJson(new FileReader("src//s_dataset.json"), InfoJson.class);
+        System.out.println(informacion);
+    }
 }
