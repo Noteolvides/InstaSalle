c6dc020039c27f96a32fd9ff1ad2d21644b0908c
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-11-28 11:01:44 +0100

    Added Final Parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,11 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Gson gson = new GsonBuilder().create();
-        User infomacionUsuarios = gson.fromJson(new FileReader("src//s_dataset.json"), User.class);
-        System.out.println(infomacionUsuarios);
+        User[] infomacionUsuarios = gson.fromJson(new FileReader("m_dataset.json"), User[].class);
+        System.out.println(infomacionUsuarios.length);
     }
 }
