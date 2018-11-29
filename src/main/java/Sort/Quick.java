424e574d933b31e8c01721aa5a83affc87e3fe2d
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-11-29 16:11:15 +0100

    Added QuickSort
    Added QuickSort, return to be finished

--- src/main/java/Sort/Quick.java
+++ src/main/java/Sort/Quick.java
@@ -1,8 +1,55 @@
 package Sort;
 
-public class Quick {
-    private Integer s;
-    private Integer t;
+import java.util.ArrayList;
 
-    //switch
+public class Quick {
+    public Integer[] QuickSort(Integer[] a, Integer i, Integer j) {
+        Integer s, t;
+
+        if (i >= j) {
+            return a;
+        }
+        if (i < j) {
+            a = particio(a, i, j);
+            a = QuickSort(a, i, t);
+            a = QuickSort(a, s, j);
+        }
+        return a;
+    }
+
+
+    //Falta corregir els returns
+    private Integer[] particio(Integer[] a, Integer i, Integer j) {
+        Integer mig, pivot, tmp, s, t;
+
+        s = i;
+        t = j;
+        mig = (i + j) / 2;
+        pivot = a[mig];
+
+        while (s <= t) {
+            while (a[s] < pivot) {
+                s++;
+            }
+            while (a[t] > pivot) {
+                t++;
+            }
+            if (s < t) {
+                tmp = a[s];
+                a[s] = a[t];
+                a[t] = tmp;
+                s++;
+                t--;
+            }
+            if (s == t) {
+                s++;
+                t--;
+            }
+            if (s > t) {
+
+            }
+        }
+
+        return a;
+    }
 }
