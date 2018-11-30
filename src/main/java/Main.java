965a15c3b25447446cb238ed883077728687505d
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-11-30 14:13:41 +0100

    Test Finalized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,8 +13,10 @@ public class Main {
         Gson gson = new GsonBuilder().create();
         User[] infomacionUsuarios = gson.fromJson(new FileReader("xs_dataset.json"), User[].class);
         Quick quick = new Quick();
-        System.out.println(infomacionUsuarios[0].getPosts());
+        //System.out.println(infomacionUsuarios[0].getPosts());
         quick.QuickSort(infomacionUsuarios[0].getPosts());
-        System.out.println(infomacionUsuarios[0].getPosts());
+        for (int i = 0; i < infomacionUsuarios[0].getPosts().size() ; i++) {
+            System.out.println(infomacionUsuarios[0].getPosts().get(i).getPublished());
+        }
     }
 }
