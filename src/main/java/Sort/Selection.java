f6039a058b08c49461ba2041774ae07caf4b7355
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-12-01 16:57:39 +0100

    Created SelectionSort

---
+++ src/main/java/Sort/Selection.java
@@ -0,0 +1,21 @@
+package Sort;
+/*
+public class Selection {
+    public static void SelectionSort(int a[], int n){
+        int minim;
+        int aux;
+
+        for(int i = 0; i < n-1; i++) {
+            minim = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minim]){
+                    minim = j;
+                }
+            }
+            aux = a[minim];
+            a[minim] = a[i];
+            a[i] = aux;
+        }
+    }
+}
+*/
