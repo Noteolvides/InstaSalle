ee0f72e965a003483da3b0de27fe9e6b35ac16f6
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-12-01 20:36:14 +0100

    Updated SelectionSort
    Changed to sort for Post

--- src/main/java/Sort/Selection.java
+++ src/main/java/Sort/Selection.java
@@ -1,21 +1,25 @@
 package Sort;
+
+import JsonClasses.Post;
+
+import java.util.List;
 /*
 public class Selection {
-    public static void SelectionSort(int a[], int n){
+    public static void SelectionSort(List<Post> a, int n){
         int minim;
-        int aux;
+        Post aux;
 
         for(int i = 0; i < n-1; i++) {
             minim = i;
             for (int j = i + 1; j < n; j++) {
-                if (a[j] < a[minim]){
+                if (a.get(j).getId() < a.get(minim).getId()){
                     minim = j;
                 }
             }
-            aux = a[minim];
-            a[minim] = a[i];
-            a[i] = aux;
+            aux = a.get(minim);
+            a.set(minim, a.get(i));
+            a.set(i, aux);
         }
     }
-}
-*/
+}*/
+
