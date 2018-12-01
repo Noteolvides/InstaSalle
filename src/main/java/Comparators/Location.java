4f4102d8381c4e5a8f0317d8881564a2b6cf49d5
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-12-01 13:31:06 +0100

    Refactor CalcularDistanciaComparator
    Casi terminado CalcularDistancia comparador

--- src/main/java/Comparators/Location.java
+++ src/main/java/Comparators/Location.java
@@ -3,14 +3,54 @@ package Comparators;
 import JsonClasses.Post;
 
 import java.util.Comparator;
+import java.util.List;
+
 
 public class Location implements Comparator<Post> {
-    private int x;
-    public Location(int x) {
-        this.x = x;
+
+    private final int RADIUS_EARTH = 6371;
+
+    public double haversin(double theta){
+        return Math.pow(Math.sin(theta/2),2);
     }
 
+    private double calculDist(List<Double> p1, List<Double> p2){
+        Double teta1 = Math.toRadians(p1.get(0));
+        Double teta2  = Math.toRadians(p2.get(0));
+
+
+        double distLati = Math.toRadians(p2.get(0) - p1.get(0));
+        double distLong = Math.toRadians(p2.get(1) - p1.get(1));
+
+        double a = haversin(distLati) + Math.cos(teta1) * Math.cos(teta2) * haversin(distLong);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double d =  RADIUS_EARTH * c;
+
+        return d;
+    }
+
+    private List<Double> location;
+
+    public Location(List<Double> location) {
+        this.location = location;
+    }
+
+
     public int compare(Post o1, Post o2) {
-        return o2.getId().compareTo(o1.getId());
+       double dist2 = calculDist(o1.getLocation(),location);
+       double dist1  = calculDist(o2.getLocation(),location);
+
+        if (dist2 == dist1){
+            return 0;
+        }
+        if (dist2 > dist1){
+            return -1;
+        }
+        if (dist2 < dist2){
+            return 1;
+        }
+        return 0;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import Comparators.Ascendent;
 import Comparators.Location;
 import JsonClasses.Post;
 import JsonClasses.User;
@@ -21,10 +20,12 @@ public class Main {
         for (User i:infomacionUsuarios) {
             posts.addAll(i.getPosts());
         }
-
-        quick.QuickSort(posts,new Location(2));
+        ArrayList<Double> location = new ArrayList<Double>();
+        location.add(41.3854363);
+        location.add(2.1730605);
+        quick.QuickSort(posts,new Location(location));
         for (int i = 0; i < posts.size() ; i++) {
-            System.out.println(posts.get(i).getPublished());
+            System.out.println(posts.get(i).getId());
         }
     }
 }
