760be5546a29417df6ce0cc0421a059f999a13d6
gustavo4g <gustavogomezlopezdesanroman@gmail.com>
2018-12-07 21:42:47 +0100

    Update Merge.java

--- src/main/java/Sort/Merge.java
+++ src/main/java/Sort/Merge.java
@@ -1,14 +1,24 @@
 package Sort;
 
 import JsonClasses.Post;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 
 //Error
+
+
 public class Merge {
+    /*
+    public static void main(String[] args) throws FileNotFoundException {
+        Gson gson = new GsonBuilder().create();
+        User[] infomacionUsuarios = gson.fromJson(new FileReader("xs_dataset.json"), User[].class);
+        Merge m = new Merge();
+        m.MergeSort(infomacionUsuarios[0].getPosts(),new Descendent());
+        System.out.println(infomacionUsuarios[0].getPosts());
+    }
+    */
    private Comparator<Post> c;
 
     public Merge() {
@@ -21,50 +31,44 @@ public class Merge {
 
     private void ordenaMerge(List<Post> a, int i, int j){
         if (i < j){
-            System.out.println("\ni" + i + "j" + j);
             int mig = (i + j) / 2;
-            System.out.println("\nmig =" + mig);
             ordenaMerge(a, i, mig);
             ordenaMerge(a,mig+1, j);
-            Sort(a, i, mig, j);
+            merge(a, i, mig, j);
         }
     }
 
-    private void Sort(List<Post> a, int i, int mig, int j){
-        List<Post> b = new ArrayList<Post>();
-        int k1, k2, cursor, kr;
-
-        k1 = i;
-        k2 = mig+1;
-        cursor = 0;
-
-        while (k1<=mig && k2<=j){
-            if(this.c.compare(a.get(k1),a.get(k2)) <= 0){
-                b.set(cursor, a.get(k1));
-                k1++;
-                cursor++;
-            }else{
-                b.set(cursor, a.get(k2));
-                k2++;
-                cursor++;
-            }
+    private void merge(List<Post> a, int i, int mig, int j){
+        int n1 = mig - i + 1;
+        int n2 = j - mig;
+        List<Post> A = new ArrayList<Post>();
+        List<Post> B = new ArrayList<Post>();
+        for (int k = 0; k < n1; k++) {
+            A.add(a.get(i+k));
         }
-        while (k1<=mig){
-            b.set(cursor, a.get(k1));
+        for (int t = 0; t < n2; t++) {
+            B.add(a.get(mig + 1 + t));
+        }
+        int k1 = 0,k2 = 0;
+        int cursor = i;
+        while (k1 < n1 && k2 < n2){
+            if (this.c.compare(A.get(k1),B.get(k2)) <= 0){
+                a.set(cursor,A.get(k1));
+                k1++;
+            }else{
+                a.set(cursor,B.get(k2));
+                k2++;
+            }
+            cursor++;
+        }
+        while (k1 < n1){
+            a.set(cursor,A.get(k1));
             k1++;
             cursor++;
         }
-        while (k2<=j){
-            b.set(cursor, a.get(k2));
+        while (k2 < n2){
+            a.set(cursor,B.get(k2));
             k2++;
-            cursor++;
-        }
-        cursor = 0;
-        kr = i;
-
-        while (kr<=j){
-            a.set(kr, b.get(cursor));
-            kr++;
             cursor++;
         }
     }
