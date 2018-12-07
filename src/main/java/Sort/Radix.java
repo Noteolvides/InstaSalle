389370dd41e1aa93cba51f2d4a3e731d604b8330
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-12-07 18:53:16 +0100

    RadixSort Sorts beach

--- src/main/java/Sort/Radix.java
+++ src/main/java/Sort/Radix.java
@@ -16,7 +16,7 @@ public class Radix {
 
     public void RadixSort(List<Post> a, Comparator<Post> c){
         this.c = c;
-        sort(a,a.size()-1);
+        sort(a,a.size());
     }
     private int maxValue(List<Post> a, int n){
         int max = a.get(0).getPublished();
@@ -29,24 +29,23 @@ public class Radix {
     }
 
     private void countSort(List<Post> a, int n, int exp){
-        List<Post> resultat = new ArrayList<Post>();
-        int counter[] = new int[10];
-        Arrays.fill(counter,0);
+        Post[] resultat = new Post[n];
+        int counter[] = new int[maxValue(a, n)];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; ++i){
             counter[(a.get(i).getPublished()/exp)%10]++;
         }
-        for (int i = 1; i < 10; i++){
+        for (int i = 1; i < n; ++i){
             counter[i] += counter[i - 1];
         }
 
         for (int i = n - 1; i>= 0; i--){
-            resultat.set(counter[(a.get(i).getPublished()/exp)%10] -1, a.get(i));
+            resultat[counter[(a.get(i).getPublished()/exp)%10] -1]= a.get(i);
             counter[(a.get(i).getPublished()/exp)%10]--;
         }
 
-        for (int i = 0; i < n; i++){
-            a.set(i, resultat.get(i));
+        for (int i = 0; i < n; ++i){
+            a.set(i, resultat[i]);
         }
     }
 
